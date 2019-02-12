6b9ff0272172f3ef18bec7dd04de486ca2df3c5b
geek10010 <36638017+geek10010@users.noreply.github.com>
2019-02-12 23:27:05 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-hw
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello CodeSandbox!!!</h1>
+//       <h2>Start **** editing to see some magic happen!</h2>
+//     </div>
+//   );
+// }
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
+
+const rootElement = document.getElementById("root");
+const props = {
+  className: "App",
+  children: "Hello World----***"
+};
+const element = <div {...props} />;
+ReactDOM.render(element, rootElement);
