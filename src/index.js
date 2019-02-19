92d60733205f4c48cdea68ca2290c324aa405394
geek10010 <36638017+geek10010@users.noreply.github.com>
2019-02-19 22:22:37 +0300

    feat: lesson 7, Rerender a React app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "./styles.css";
+import './styles.css';
 
 // function App() {
 //   return (
@@ -15,22 +15,41 @@ import "./styles.css";
 // const rootElement = document.getElementById("root");
 // ReactDOM.render(<App />, rootElement);
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 // step 02
 const props = {
-  className: "App",
-  children: ["Hello World!", "and people", <div id="step2" />]
+  className: 'App',
+  children: <div id="step2" />
 };
 const element = <div {...props} />;
 ReactDOM.render(element, rootElement);
 
 // step 03
-const step2Element = document.getElementById("step2");
+const step2Element = document.getElementById('step2');
 const Message = props => <div>{props.msg}</div>;
 const element2 = (
   <div className="container">
     <Message msg="Hello 2" />
     <Message msg="Goodbye!" />
+    <div id="step4" />
   </div>
 );
 ReactDOM.render(element2, step2Element);
+
+// step 04 - lesson 7, rerender a React app
+const step4 = document.getElementById('step4');
+
+function tick() {
+  const time = new Date().toLocaleTimeString();
+  const element4 = (
+    <div>
+      It is
+      <input value={time} />
+      <input value={time} />
+    </div>
+  );
+
+  ReactDOM.render(element4, step4);
+}
+
+setInterval(tick, 1000);
